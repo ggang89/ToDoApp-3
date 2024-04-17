82b7cb3f0fe29bc11b633d32e79c55411ea46321
ggang89 <ggang89@gmail.com>
2024-04-17 16:02:51 +0900

    기본 구조 완성(input창과 list)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("todoapp만들기");
   return (
-   <>
-   </>
+    <div className="wrap">
+      <h1 className="title">To Do App 3</h1>
+      <div className="addTodo">
+        <label for="todo">NEW ToDo </label>
+        <input
+          id="todo"
+          type="text"
+          size="50"
+          placeholder="Add Todo..."
+        ></input>
+        <button>추가</button>
+      </div>
+
+      <ul className="todolistBox">
+        <li className="todolist">
+          <p>{todo}</p>
+          <button>수정</button>
+          <button>삭제</button>
+        </li>
+        <li className="todolist">
+          <p>{todo}</p>
+          <button>수정</button>
+          <button>삭제</button>
+        </li>
+      </ul>
+    </div>
   );
 }
 
